7481abfe66f87221341a92b913cede2b96112d42
atozfreight <atozfreight.in.k@gmail.com>
2021-05-17 23:56:10 +0530

    first

---
+++ src/main/java/xyz/atozfreight/AtoZFreightApplication.java
@@ -0,0 +1,13 @@
+package xyz.atozfreight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtoZFreightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtoZFreightApplication.class, args);
+	}
+
+}
---
+++ src/main/java/xyz/atozfreight/User.java
@@ -0,0 +1,61 @@
+package xyz.atozfreight;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+    @Column(nullable = false, length = 64)
+    private String password;
+    @Column(nullable = false, length = 40)
+    private String firstName;
+    @Column(nullable = false, length = 40)
+    private String lastName;
+
+}
